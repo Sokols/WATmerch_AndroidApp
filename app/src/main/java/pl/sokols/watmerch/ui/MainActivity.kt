53ff336f094fc974a873fa4ee4dcc74f6aff2aa4
Igor Sokół <asus.11@tlen.pl>
2021-04-16 16:27:54 +0200

    Added styling the application by theme and changing theme by onClick

--- app/src/main/java/pl/sokols/watmerch/ui/MainActivity.kt
+++ app/src/main/java/pl/sokols/watmerch/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package pl.sokols.watmerch.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import pl.sokols.watmerch.R
@@ -17,12 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bottomNavigation.setupWithNavController(
-            Navigation.findNavController(
-                this,
-                R.id.nav_host_fragment
-            )
-        )
+        setNavigationAndMenu()
     }
 
     override fun onBackPressed() {
@@ -34,5 +31,37 @@ class MainActivity : AppCompatActivity() {
             ) { _, _ -> super@MainActivity.onBackPressed() }
             .setNegativeButton(getString(R.string.no), null)
             .show()
+    }
+
+    private fun setNavigationAndMenu() {
+        binding.bottomNavigation.setupWithNavController(
+            Navigation.findNavController(
+                this,
+                R.id.nav_host_fragment
+            )
+        )
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.changeTheme -> {
+                    when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                        Configuration.UI_MODE_NIGHT_YES ->
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        Configuration.UI_MODE_NIGHT_NO ->
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    true
+                }
+                R.id.search -> {
+                    // Handle search icon press
+                    true
+                }
+                R.id.more -> {
+                    // Handle more item (inside overflow menu) press
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
